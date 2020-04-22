5677b21cd97a7d3ce72eb15288e4448e1b4bf145
awaisshaikh94@gmail.com <awaisshaikh94@gmail.com>
2020-04-22 22:10:56 +0500

    prodcuts and types controller init with CRUD methods

---
+++ app/controllers/v1/products_controller.rb
@@ -0,0 +1,44 @@
+class V1::ProductsController < ApplicationController
+
+  def index
+    products = Product.all
+    render json: { products: products }, status: :ok
+  end
+
+  def create
+    product = Product.new(product_params)
+
+    if product.save
+      render json:product, status: :created
+    else
+      render json: {errors: product.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    product = Product.find(params[:id])
+    render json: product, status: :ok
+  end  
+
+  def update
+    product = Product.find(params[:id])
+    if product.update(product_params)
+      render json: product, status: :ok
+    else
+      render json: { errors: product.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    head 204
+  end
+
+  private
+  
+  def product_params
+    params.require(:product).permit(:name, :description, :image)
+  end
+
+end
---
+++ app/controllers/v1/types_controller.rb
@@ -0,0 +1,44 @@
+class V1::TypesController < ApplicationController
+  
+  def index
+    types = Type.all
+    render json: { types: types }, status: :ok
+  end
+
+  def create
+    type = Type.new(type_params)
+
+    if type.save
+      render json:type, status: :created
+    else
+      render json: {errors: type.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    type = Type.find(params[:id])
+    render json: type, status: :ok
+  end  
+
+  def update
+    type = Type.find(params[:id])
+    if type.update(type_params)
+      render json: type, status: :ok
+    else
+      render json: { errors: type.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    type = Type.find(params[:id])
+    type.destroy
+    head 204
+  end
+
+  private
+  
+  def type_params
+    params.require(:type).permit(:name, :description, :image)
+  end
+
+end
