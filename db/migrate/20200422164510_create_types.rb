572ee1964858916e5442edb293bf3551129d9e82
awaisshaikh94@gmail.com <awaisshaikh94@gmail.com>
2020-04-22 21:51:36 +0500

    product and type model creation

---
+++ db/migrate/20200422164510_create_types.rb
@@ -0,0 +1,11 @@
+class CreateTypes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :types do |t|
+      t.string :name
+      t.integer :amount
+      t.references :product, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
