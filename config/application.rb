e4bbfa801ee8ac4ff09d8d3bacd5d7f3a664761a
awaisshaikh94@gmail.com <awaisshaikh94@gmail.com>
2020-04-23 10:34:30 +0500

    CORS fix

--- config/application.rb
+++ config/application.rb
@@ -32,14 +32,10 @@ module PizzaDelivery
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource(
-          '*',
-          headers: :any,
-          methods: [:get, :patch, :put, :delete, :post, :options]
-          )
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
     
